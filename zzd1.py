e4c795c01aa1e7289fe2849bdcd33e4b4ff56665
py-46-Alex <90619667+py-46-Alex@users.noreply.github.com>
2023-05-29 19:13:40 +0300

    Update zzd1-2.py
    3aDaHue TeCToBoe 1.2

--- zzd1.py
+++ zzd1.py
@@ -1,8 +1,6 @@
 import collections
 import asyncio
 import csv
-from datetime import datetime
-
 
 def listmaker():
     """создаст лист из айдишников
@@ -18,16 +16,10 @@ def listmaker():
         all_id.append(id)
     return all_id
 
-async def find_duplicates(lst):
-    """на вход принимает лист из наших идишников.
-    На выход отдаст только те айдишники  которые повторялись в списке 3 раза
-    асинхронна. чтобы видеть глазами нужо евейтить"""
-    counter = collections.Counter(lst)
-    # print(type(counter))
-    duplicates = [id for id in counter if counter[id] == 3]
-    return duplicates
-
 async def find_elses(lst):
+    """ на вход берет лист айдишников отдает словарь
+     где ключ это частотность а значение - количество уникальных
+     идишников из входного листа"""
     counter = collections.Counter(lst)
     elses = {}
     todict = dict(counter)
@@ -38,27 +30,17 @@ async def find_elses(lst):
                 elses[frequency].append(id)
             elif frequency in elses:
                 elses[frequency].append(id)
-    # return elses.items()
     finish_view = {}
     for frequency, ids in elses.items():
         finish_view[frequency] = len(ids)
     return finish_view.items()
 
-
 my_list = listmaker()
 
 async def main():
-    duplicates = await find_duplicates(my_list)
-    print("Айдишники, повторяющиеся три раза: ", len(duplicates))
     frequency = await find_elses(my_list)
     for fr, kol in frequency:
         print(f'вот такое количество уникальных айдишников {kol} светилось в файле с частотой {fr}')
 
-    print('________________________________________________________________________________________________________')
-
 if __name__ == '__main__':
-    # start = datetime.now()
     asyncio.run(main())
-    # print('текущее время работы программы')
-    # print(datetime.now() - start)
-    print('________________________________________________________________________________________________________')
